9417bb78be226cc957639ed96c4e6a66d8238fbe
Jin Zhanyu <851162585@qq.com>
2018-11-18 16:36:29 +0800

    change rm_record

--- src/rm_record.cc
+++ src/rm_record.cc
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include "rid.h"
 #include "rm.h"
 #include "rm_error.h"
@@ -8,25 +8,14 @@
 #define INIT_REC_PAGE  -1 
 #define INIT_REC_SLOT  -1
 
-=======
-#include "rm_rid.h"
-#include "rm.h"
-#include <iostream>
-
->>>>>>> 1a202fac15c28d4232e5bc80f8bd9d650fdb164a
 using namespace std;
 
 RM_Record::RM_Record()
 {
-<<<<<<< HEAD
     size = INIT_REC_SIZE;
     data = NULL;
     rid = RID(INIT_REC_PAGE,INIT_REC_SLOT);
-=======
-    size = -1;
-    data = NULL;
-    rid = RID(-1,-1);
->>>>>>> 1a202fac15c28d4232e5bc80f8bd9d650fdb164a
+
 }
 
 RM_Record::~RM_Record()
@@ -43,11 +32,8 @@ RC RM_Record::GetData(char *&pData) const{
 		return 0;
 	}
 	else 
-<<<<<<< HEAD
 		return RM_NULLRECORD; // todo:需要定义一个record为空的错误编号
-=======
-		return -1; // todo:需要定义一个record为空的错误编号
->>>>>>> 1a202fac15c28d4232e5bc80f8bd9d650fdb164a
+
 }
 
 RC RM_Record::GetRid (RID &rid) const 
@@ -58,11 +44,7 @@ RC RM_Record::GetRid (RID &rid) const
         return 0;
     }
 	else 
-<<<<<<< HEAD
 		return RM_NULLRECORD;
-=======
-		return -1;
->>>>>>> 1a202fac15c28d4232e5bc80f8bd9d650fdb164a
 }
 
 
